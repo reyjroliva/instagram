3218e11a028164afd049a04bdf6747f0422712c3
Rey Oliva <reyjroliva@dhcp-172-20-142-128.dhcp.thefacebook.com>
2017-06-27 15:58:04 -0700

    Server set up, Login view controller set up with sign in and sign up

--- instagram/AppDelegate.swift
+++ instagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "instagram"
+                configuration.clientKey = "#$Ubv8nw058vrbui"
+                configuration.server = "https://stormy-caverns-74158.herokuapp.com//parse"
+            })
+        )
         return true
     }
 
---
+++ instagram/LoginViewController.swift
@@ -0,0 +1,68 @@
+//
+//  LoginViewController.swift
+//  instagram
+//
+//  Created by Rey Oliva on 6/27/17.
+//  Copyright © 2017 Rey Oliva. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        PFUser.logInWithUsername(inBackground: usernameField.text!, password: passwordField.text!) { (user: PFUser?, error: Error?) in
+            if user != nil {
+                print("You're logged in")
+            }
+        }
+    }
+    
+    @IBAction func onSIgnUp(_ sender: Any) {
+        let newUser = PFUser()
+        
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if success {
+                print("Created a user")
+            } else {
+                print(error?.localizedDescription as Any)
+                if (error! as NSError).code == 202 {
+                    print("Username is taken")
+                }
+            }
+        }
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
